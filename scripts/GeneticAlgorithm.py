ebcbb1c5325b3cb2497b77809fe78ae58ee81e22
Daniele Reis <danielesreis@outlook.com>
2019-04-11 23:19:29 -0300

    finished GA

--- scripts/GeneticAlgorithm.py
+++ scripts/GeneticAlgorithm.py
@@ -1,15 +1,46 @@
+import pandas as pd
 import numpy as np
 import math
 import random
 
 class GeneticAlgorithm():
 
-    def __init__(self, population_size, mutation_rate, iterations, runs, max_var):
+    def __init__(self, model, population_size, mutation_rate, iterations_number, max_var, variables, crossover_prob):
+        self.model = model
         self.population_size = population_size
         self.mutation_rate = mutation_rate
-        self.iterations = iterations
-        self.runs = runs
+        self.iterations_number = iterations_number
         self.max_var = max_var
+        self.variables = variables
+        self.crossover_prob = crossover_prob
+        self._cromossomes = None
+        self._new_generation = None
+        self._fitness_values = None
+        random.seed(0)
+
+    @property
+    def new_generation(self):
+        return self._new_generation
+
+    @new_generation.setter
+    def new_generation(self, new_generation):
+        self._new_generation = new_generation
+
+    @property
+    def cromossomes(self):
+        return self._cromossomes
+
+    @cromossomes.setter
+    def cromossomes(self, cromossomes):
+        self._cromossomes = cromossomes
+
+    @property
+    def fitness_values(self):
+        return self._fitness_values
+
+    @fitness_values.setter
+    def fitness_values(self, fitness_values):
+        self._fitness_values = fitness_values
         
     # flow: 
     # generate a population with size equal to population_size (how to generate this pop? randomly? or from potentially good wavelengths?)
@@ -20,3 +51,76 @@ class GeneticAlgorithm():
     # reproduce them (how?)
     # mutation on the new generation
     # elitism maybe?
+
+    def best_cromossome(self):
+        return self.fitness_values['fitness'].sort_values(ascending=False).iloc[0].index.value
+
+    def initial_pop(self):
+        cromossomes = pd.DataFrame(index=np.arange(self.population_size), columns=self.variables, data=0)
+        for cromossome in range(self.population_size):
+            rnd_wvl = random.sample(set(self.variables), self.max_var)
+            cromossomes.iloc[cromossome][rnd_wvl] = 1
+        return cromossomes
+
+    def fit_model(self):
+        fitness_values = np.array([0]*self.population_size)
+
+        for cromossome in range(self.population_size):
+            selected_wvls = self.variables[self.cromossomes.iloc[cromossome].values == 1]
+            fitness_values[cromossome] = self.model.build(selected_wvls)
+
+        self.fitness_values = pd.DataFrame(index=np.arange(self.population_size), columns=['cromossome', 'fitness'])
+        self.fitness_values['cromossome'] = self.cromossomes
+        self.fitness_values['fitness'] = fitness_values
+
+    def selection(self):
+        cromossomes = random.sample(set(np.arange(self.population_size)), 4)
+        df = pd.DataFrame(index=np.arange(4), columns=['cromossome', 'fitness'])
+        df['cromossome'] = cromossomes
+        df['fitness'] = self.fitness_values[cromossomes]
+        best_cromossomes = df['fitness'].sort_values(ascending=False).iloc[:2].index.values
+
+        return best_cromossomes
+
+    def reproduction(self, selected):
+        prob = self.crossover_prob * 100
+        assert type(prob) is not int, 'A probabilidade do crossover deve ser inteira!'
+
+        mask = pd.Series(np.array([0]*len(self.variables)))
+        func = lambda x: int(not x) if random.randint(0, 99) < prob else x
+        mask = mask.apply(func).values
+        mask = int(''.join(str(val) for val in mask), 2)
+        inv_mask = 2**len(self.variables)-1 - mask
+
+        selected_1 = self.variables[selected[0]]
+        selected_2 = self.variables[selected[1]]
+
+        selected_1 = selected_1 & mask
+        selected_2 = selected_2 & inv_mask
+        child = selected_1 | selected_2
+        child = [int(val) for val in bin(child)[2:]]
+        return pd.Series([0]*(len(self.variables) - len(child)) + child)
+
+    def mutation(self, cromossome):
+        prob = self.mutation_rate * 100
+        assert type(prob) is not int, 'A taxa de mutacao deve ser inteira!'
+        return cromossome.apply(lambda x: int(not x) if random.randint(0, 99) == prob else x)
+
+    def increase_generation(self, index, cromossome):
+        self.new_generation.iloc[index] = cromossome
+
+    def otimize(self):
+        self.cromossomes = self.initial_pop()
+
+        for __ in range(self.iterations_number):
+            self.fit_model()
+            for cromossome in range(self.population_size):
+                selected = self.selection()
+                new_cromossome = self.reproduction(selected)
+                mutated = self.mutation(new_cromossome)
+                self.increase_generation(cromossome, mutated)
+            self.cromossomes = self.new_generation
+
+        best_cromossome = self.best_cromossome()
+
+        return self.variables[self.cromossomes.iloc[best_cromossome].values == 1]
--- scripts/Training.py
+++ scripts/Training.py
@@ -1,6 +1,7 @@
 
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import explained_variance_score
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 import numpy as np
@@ -10,46 +11,56 @@ import math
 
 class Training():
 
-    def optimal_lv_num(self, X, Y, max_iter=100, scale=True):
-        rmsecv = np.array([])
-        for lv in np.arange(10):
-            pls = PLSRegression(lv, max_iter, scale)
-            rmsecv = np.append(rmsecv, math.sqrt(abs(cross_val_score(pls, X, Y, scoring='neg_mean_squared_error', cv=10).mean())))
+	def __init__(self, max_iter, scale=True):
+		self.max_iter = max_iter
+		self.scale = scale
+		self._lv_number = None
 
-        optimal = np.where(rmsecv==min(rmsecv))[0][0] + 1
-        return optimal
+	@property
+	def lv_number(self):
+		return self._lv_number
 
-    def pls(self, X, Y, lv_number, k, max_iter=100, scale=True, cross_validate):
-        pls = PLSRegression(lv_number, max_iter, scale)
+	@lv_number.setter
+	def lv_number(self, lv_number):
+		self._lv_number = lv_number
 
-        if(cross_validate):
-            kf = KFold(n_splits=k, shuffle=True, random_state=42)
-            metrics = pd.DataFrame(columns=['R2', 'MSE'])
+	def optimal_lv_num(self, X, Y):
+		rmsecv = np.array([])
+		for lv in np.arange(10):
+			pls = PLSRegression(lv, self.max_iter, self.scale)
+			rmsecv = np.append(rmsecv, math.sqrt(abs(cross_val_score(pls, X, Y, scoring='neg_mean_squared_error', cv=10).mean())))
 
-            for train_i, test_i in kf.split(X):
-                X_train, X_test = X.iloc[train_i], X.iloc[test_i]
-                Y_train, Y_test = Y.iloc[train_i], Y.iloc[test_i]
+		optimal = np.where(rmsecv==min(rmsecv))[0][0] + 1
+		self.lv_number = optimal
 
-                pls.fit(X_train, Y_train)
-                Y_pred = pls.predict(X_test)
+	def pls(self):
+		return PLSRegression(self.lv_number, self.max_iter, self.scale)
 
-                r2 = r2_score(Y_test, Y_pred)
-                mse = mean_squared_error(Y_test, Y_pred)
-                metrics = metrics.append({'R2': r2, 'MSE': mse}, ignore_index=True)
+	def pls_cv(self, X, Y, k):
+		pls = self.pls()
+		kf = KFold(n_splits=k, shuffle=True, random_state=42)
+		metrics = pd.DataFrame(columns=['R2', 'MSE'])
 
-            metrics = metrics.append(metrics.mean(), ignore_index=True)
-            indexes = list(metrics.index[:-1].values+1) + ['mean']
-            metrics.index = indexes
-        else:
-            rmsecv = math.sqrt(abs(cross_val_score(pls, X, Y, scoring='neg_mean_squared_error', cv=10).mean()))
-            metrics = rmsecv
+		for train_i, test_i in kf.split(X):
+			X_train, X_test = X.iloc[train_i], X.iloc[test_i]
+			Y_train, Y_test = Y.iloc[train_i], Y.iloc[test_i]
+			pls.fit(X_train, Y_train)
+			Y_pred = pls.predict(X_test)
+			r2 = r2_score(Y_test, Y_pred)
+			mse = mean_squared_error(Y_test, Y_pred)
+			metrics = metrics.append({'R2': r2, 'MSE': mse}, ignore_index=True)
 
-        return metrics
+		metrics = metrics.append(metrics.mean(), ignore_index=True)
+		indexes = list(metrics.index[:-1].values+1) + ['mean']
+		metrics.index = indexes
+		return metrics
 
-    def predict(self, model, X_test, Y_test):
-        Y_pred = model.predict(X_test)
+	def build(self):
 
-        r2 = r2_score(Y_test, Y_pred)
-        mse = mean_squared_error(Y_test, Y_pred)
+	def predict(self, model, X_test, Y_test):
+		Y_pred = model.predict(X_test)
 
-        return r2, mse
+		r2 = r2_score(Y_test, Y_pred)
+		mse = mean_squared_error(Y_test, Y_pred)
+
+		return r2, mse
